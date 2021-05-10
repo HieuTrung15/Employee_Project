c11b3b8255a4eb3cb1072306174383bc864e0ed4
HieuTrung15 <hieutrungvietnam0107@gmail.com>
2021-05-10 09:19:22 +0700

    change code style

--- employee_register/views.py
+++ employee_register/views.py
@@ -15,21 +15,21 @@ def employee_form(request, id=0):
         if id == 0:
             form = EmployeeForm()
         else:
-            employee = Employee.objects.get(pk=id)
+            employee = Employee.objects.get(id=id)
             form = EmployeeForm(instance=employee)
         return render(request, "employee_register/employee_form.html", {'form': form})
     else:
         if id == 0:
             form = EmployeeForm(request.POST)
         else:
-            employee = Employee.objects.get(pk=id)
-            form = EmployeeForm(request.POST,instance= employee)
+            employee = Employee.objects.get(id=id)
+            form = EmployeeForm(request.POST, instance=employee)
         if form.is_valid():
             form.save()
         return redirect('/employee/list')
 
 
-def employee_delete(request,id):
-    employee = Employee.objects.get(pk=id)
+def employee_delete(request, id):
+    employee = Employee.objects.get(id=id)
     employee.delete()
     return redirect('/employee/list')
